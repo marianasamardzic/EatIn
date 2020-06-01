ca7fc434e4c064b1e6ae0210d55b45692578446c
Mariana Samardzic <marianasamardzic2@gmail.com>
2020-06-01 15:58:34 +0200

    Project setup

---
+++ src/main/java/com/eatin/EatinApplication.java
@@ -0,0 +1,13 @@
+package com.eatin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatinApplication.class, args);
+	}
+
+}
