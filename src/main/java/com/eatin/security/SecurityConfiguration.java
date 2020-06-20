5993e64f5e82f33d9840a5a4cff1e0fd59af9d47
nteske <nemanjatesic96@gmail.com>
2020-06-20 17:28:28 +0200

    taking down cors protection

--- src/main/java/com/eatin/security/SecurityConfiguration.java
+++ src/main/java/com/eatin/security/SecurityConfiguration.java
@@ -47,8 +47,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
-		web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui", "/swagger-resources/**",
-				"/configuration/security", "/swagger-ui.html", "/webjars/**");
+		web.ignoring().antMatchers("/**");
 	}
 
 	@Bean
