64d80210110c06f7d9b514f92a958367b9feb4fd
Mariana Samardzic <marianasamardzic2@gmail.com>
2020-07-09 21:26:57 +0200

    deleted file controller

--- src/main/java/com/eatin/EatinApplication.java
+++ src/main/java/com/eatin/EatinApplication.java
@@ -2,11 +2,7 @@ package com.eatin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-import com.eatin.storage.FileStorageProperties;
-
-@EnableConfigurationProperties({ FileStorageProperties.class })
 @SpringBootApplication
 public class EatinApplication {
 
