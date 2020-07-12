a37824f1bd95f5f7df496ff11eb209a324679ce3
Mariana Samardzic <marianasamardzic2@gmail.com>
2020-07-12 16:27:06 +0200

    put tip restorana

--- src/main/java/com/eatin/controllers/TipRestoranaController.java
+++ src/main/java/com/eatin/controllers/TipRestoranaController.java
@@ -2,18 +2,21 @@ package com.eatin.controllers;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eatin.common.ObjectMapperUtils;
 import com.eatin.dto.restoran.TipRestoranaDTO;
 import com.eatin.dto.restoran.TipRestoranaNoIdDTO;
+import com.eatin.error.CustomException;
 import com.eatin.jpa.Tip_restorana;
 import com.eatin.repository.TipRestoranaRepository;
 
@@ -42,5 +45,17 @@ public class TipRestoranaController {
 		tipEntity.setOpisTipaRestorana(tip.getOpisTipaRestorana());
 		this.tipRestoranaRepository.save(tipEntity);
 		return new ResponseEntity<TipRestoranaNoIdDTO>(tip, HttpStatus.CREATED);
+	}
+
+	@PutMapping("tip-restorana-admin")
+	@ApiOperation("Menja tip restorana")
+	public ResponseEntity<TipRestoranaDTO> updateTipRestorana(@RequestBody TipRestoranaDTO tip) throws Exception {
+		Optional<Tip_restorana> entity = this.tipRestoranaRepository.findById(tip.getIdTipaRestorana());
+		if (!entity.isPresent()) {
+			throw new CustomException("Ne postoji tip restorana sa zadatim id-jem");
+		}
+		entity.get().setOpisTipaRestorana(tip.getOpisTipaRestorana());
+		this.tipRestoranaRepository.save(entity.get());
+		return new ResponseEntity<TipRestoranaDTO>(tip, HttpStatus.OK);
 	}
 }
